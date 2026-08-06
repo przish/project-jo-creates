957b51e8f840c6ce6af41811000125e307a2efb7
przish <irish_may_pureza@dlsl.edu.ph>
2026-08-06 18:44:47 +0800

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { Inter, Montserrat } from 'next/font/google';
 import './globals.css';
 
@@ -11,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={`${inter.className} ${montserrat.className}`}>
         {children}
       </body>
